266fc5d13673fd4db5a707aba6565be2014fc636
Anup Sure <sure.anup06@gmail.com>
2026-07-29 09:30:46 +0530

    Added Multiply Divide and Clear button

--- First.java
+++ First.java
@@ -4,48 +4,97 @@ import java.awt.event.*;
 class TextFields implements ActionListener
 {
     TextField t1,t2,t3;
-    Button b1,b2;
+    Button b1,b2,b3,b4,b5;
     Frame f;
     TextFields()
     {
-    f=new Frame();
-    b1=new Button("Add");
-    b1.setBounds(50,200,50,20);
-    b2=new Button("Subtract");
-    b2.setBounds(150,200,50,20);
-    t1=new TextField();
-    t1.setBounds (30,50,200,20);
-    t2=new TextField();
-    t2.setBounds (30,100,200,20);
-    t3=new TextField();
-    t3.setEditable(false);
-    t3.setBounds (30,150,200,20);
-    f.add(b1);
-    f.add(b2);
-    f.add(t1);
-    f.add(t2);
-    f.add(t3);
-    b1.addActionListener(this);
-    b2.addActionListener(this);
-    f.setSize(300,300);
-    f.setLayout(null);
-    f.setVisible(true);
-    } 
+
+        f=new Frame();
+    
+        b1=new Button("Add");
+        b1.setBounds(50,200,50,20);
+        b1.addActionListener(this);
+
+        b2=new Button("Subtract");
+        b2.setBounds(150,200,50,20);
+        b2.addActionListener(this);
+        
+        b3 = new Button("Multiply");
+        b3.setBounds(50,250,50,20);
+        b3.addActionListener(this);
+
+        b4=new Button("Divide");
+        b4.setBounds(150,250,50,20);
+        b4.addActionListener(this);
+
+        b5 = new Button("Clear");
+        b5.setBounds(225,225,50,20);
+        b5.addActionListener(this);
+
+        t1=new TextField();
+        t1.setBounds (30,50,200,20);
+        
+        t2=new TextField();
+        t2.setBounds (30,100,200,20);
+        
+        t3=new TextField();
+        t3.setEditable(false);
+        t3.setBounds (30,150,200,20);
+        
+        f.add(b1);
+        f.add(b2);
+        f.add(b3);
+        f.add(b4);
+        f.add(b5);
+
+        f.add(t1);
+        f.add(t2);
+        f.add(t3);
+
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
+    
+    }  
     public void actionPerformed(ActionEvent e)
     {
-        int a = Integer.parseInt(t1.getText());
-        int b = Integer.parseInt(t2.getText());
+        float a = Float.parseFloat(t1.getText());
+        float b = Float.parseFloat(t2.getText());
+
         if(e.getSource()==b1)
         {
-            int c = a + b;
+            float c = a + b;
             t3.setText(String.valueOf(c));
         }
-        else 
+        else if(e.getSource()==b2)
         {
-            int c = a - b;
+            float c = a - b;
             t3.setText(String.valueOf(c));
         }
+        else if(e.getSource()==b3)
+        {
+            float c = a * b;
+            t3.setText(String.valueOf(c));
+        }
+        else if(e.getSource()==b4)
+        {
+            if(b == 0)
+            {
+                t3.setText("Error: Division By Zero Detected");
+            }
+            else
+            {
+                float c = a/b;
+                t3.setText(String.valueOf(c));
+            }
+        }
+        else if(e.getSource()==b5)
+        {
+            t1.setText("");
+            t2.setText("");
+            t3.setText("");
 
+        }
     }
     public static void main(String args[])
     {
